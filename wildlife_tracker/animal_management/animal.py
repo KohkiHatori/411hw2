7e3ac5bbe0731e2595aead7eb8faac34624c2359
kohkhatori <kouki20040124hatori@gmail.com>
2024-10-06 22:33:59 -0400

    finished

--- wildlife_tracker/animal_management/animal.py
+++ wildlife_tracker/animal_management/animal.py
@@ -2,4 +2,12 @@ from typing import Any, Optional
 
 class Animal:
 
-    pass
+    def __init__(self,
+                 animal_id: int) -> None:
+        self.animal_id = animal_id
+
+    def update_animal_details(self, **kwargs: Any) -> None:
+        pass
+
+    def get_animal_details(animal_id) -> dict[str, Any]:
+        pass
--- wildlife_tracker/formal_specification.py
+++ wildlife_tracker/formal_specification.py
@@ -1,4 +1,8 @@
 from typing import Any, List, Optional
+from wildlife_tracker.migration_tracking.migration import Migration
+from wildlife_tracker.migration_tracking.migration_path import MigrationPath
+from wildlife_tracker.habitat_management.habitat import Habitat
+from wildlife_tracker.animal_management.animal import Animal
 
 
 age: Optional[int] = None
@@ -25,7 +29,6 @@ species: str
 start_date: str
 start_location: Habitat
 status: str = "Scheduled"
-
 
 def assign_animals_to_habitat(animals: List[Animal]) -> None:
     pass
